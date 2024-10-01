44067e3aa6cf2dd1c54674d11ddcdfb1c21649bb
Ari <168293205+AriRainbow@users.noreply.github.com>
2024-09-30 20:07:30 -0500

    Update TestBaseModel to handle kwargs

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -31,5 +31,31 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(model_dict['created_at'], str)
         self.assertIsInstance(model_dict['updated_at'], str)
 
+    def test_init_with_kwargs(self):
+        """Test creating ann instance from a dictionary (kwargs)."""
+        # Create an initial instance
+        base = BaseModel()
+        base.name = "My First Model"
+        base.my_number = 89
+        base_dict = base.to_dict()
+
+        # Create a new instance using dictionary (kwargs)
+        new_base = BaseModel(**base_dict)
+
+        # Check if the IDs are the same
+        self.assertEqual(base.id, new_base.id)
+
+        # Check if the create_at and updated_at are converted back to datetime
+        self.assertEqual(base.created_at, new_base.created_at)
+        self.assertEqual(base.my_number, new_base.my_number)
+
+    def test_init_no_kwargs(self):
+        """Test creating an instance without kwargs (normal initialization)."""
+        base = BaseModel()
+        self.assertIsInstance(base, BaseModel)
+        self.assertIsInstance(base.id, str)
+        self.assertIsInstance(base.created_at, datetime)
+        self.assertIsInstance(base.updated_at, datetime)
+
 if __name__ == "__main__":
     unittest.main()
