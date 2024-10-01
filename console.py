553e7efa8e617c877aaae6b21f71ea6ffffea58f
NatalieR89 <natalieritch89@gmail.com>
2024-09-30 20:31:48 -0500

    console/rm comments

--- console.py
+++ console.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
 
 
-"""
-Module
-"""
+"""Module"""
 
 
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """
-    Class
-    """
+    """Class"""
 
-    
+
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program"""
         print()
         return True
 
     def emptyline(self):
-        """Do nothing on empty input line"""
         pass
 
 if __name__ == '__main__':
