6fb77a337b2c33d5701a24be528bb0177de32c22
NatalieR89 <natalieritch89@gmail.com>
2024-10-01 20:03:09 -0500

    pycode fixes

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class that inherits from BaseModel."""
-    
+
     def __init__(self, *args, **kwargs):
         """Initialize the User instance."""
         super().__init__(*args, **kwargs)  # Call BaseModel's initializer
